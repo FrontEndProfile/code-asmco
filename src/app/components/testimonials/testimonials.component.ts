bcbea6bcafa02b3a361d7c9e1e7293b1dbf2f450
FrontEndProfile <frontendprofile@gmail.com>
2026-02-01 21:10:38 +0500

    update new chnages

--- src/app/components/testimonials/testimonials.component.ts
+++ src/app/components/testimonials/testimonials.component.ts
@@ -19,7 +19,7 @@ export class TestimonialsComponent implements OnInit {
   testimonials: Testimonial[] = [
     {
       quoteHtml:
-        '“Julia was enthusiastic and <strong>ready to jump right in to several projects ranging from UI and visual design to QA work on some of our mobile applications.</strong> Julia worked well with the team, and was also able to work autonomously when needed. I look forward to seeing what Julia\'s next UX endeavor will be!”',
+        'Fazal was enthusiastic and <strong>ready to jump right in to several projects ranging from UI and visual design to QA work on some of our mobile applications.</strong> Fazal worked well with the team, and was also able to work autonomously when needed. I look forward to seeing what Fazal\'s next UX endeavor will be!”',
       authorLine:
         '-Sara Mastro, VP, Operations and Product Management at Perceptronics Solutions, Inc',
       avatar:
@@ -28,7 +28,7 @@ export class TestimonialsComponent implements OnInit {
     },
     {
       quoteHtml:
-        '“With the help of Julia’s designs, <strong>our e-commerce site went from outdated, and underused to an additional point of sale that has allowed us to reach customers virtually that we wouldn’t have been able to reach previously.</strong> At every step of the way Julia was professional, dedicated, and a complete pleasure to work with.”',
+        '“With the help of Fazal designs, <strong>our e-commerce site went from outdated, and underused to an additional point of sale that has allowed us to reach customers virtually that we wouldn’t have been able to reach previously.</strong> At every step of the way Fazal was professional, dedicated, and a complete pleasure to work with.”',
       authorLine: '-Santiago Guzmán, CEO, Adriano',
       avatar:
         'https://cdn.prod.website-files.com/68f32de327233c470b1e6a87/6929f88d8e9ff6a515c1a403_client%20image.avif',
@@ -36,7 +36,7 @@ export class TestimonialsComponent implements OnInit {
     },
     {
       quoteHtml:
-        '“Julia is a very talented and hard-working designer... <strong>We found Julia to be extremely professional and easy to work with. Without question, we would hire her again.”</strong>',
+        'Fazal is a very talented and hard-working designer... <strong>We found Fazal to be extremely professional and easy to work with. Without question, we would hire her again.”</strong>',
       authorLine: '-Helen Wood, Conservation Montgomery Board Member',
       avatar:
         'https://cdn.prod.website-files.com/68f32de327233c470b1e6a87/6929f88dfda9ac362e189272_client%20image-2.avif',
